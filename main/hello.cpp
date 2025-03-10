a36160449d17612bb2b1b04d64ed04364e61e46f
Sushmita Sree <sureemi5489@gmail.com>
2025-03-10 22:09:15 +0530

    modified the hello.cpp file for the third time

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,7 +6,8 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins, this is PES2UG22CS606.\n";
   cout << "I have successfully built and run this . \n";
-  cout << "I have added this line after addign webhook.\n";
+  cout << "I have added this line after adding webhook.\n";
   cout << "I am trying this line again.\n";
+  cout << "this is the third time.\n"
   return 0;
 }
